b17fcec5752ac14f808bd2460777752547c3848b
Hunter Luisi <hluisi@gmail.com>
2015-02-13 01:55:53 -0800

    small formatting changes

--- include/CinderImGui.h
+++ include/CinderImGui.h
@@ -33,16 +33,17 @@
 
 // forward declarations
 namespace cinder {
-  typedef std::shared_ptr<class DataSource>	DataSourceRef;
-  namespace app {
-    typedef std::shared_ptr<class Window>   WindowRef;
-  }
-  namespace gl {
-    typedef std::shared_ptr<class Texture>		TextureRef;
-    //typedef Texture2dRef							TextureRef;
-  }
+    typedef std::shared_ptr<class DataSource>	DataSourceRef;
+    namespace app {
+        typedef std::shared_ptr<class Window>   WindowRef;
+    }
+    namespace gl {
+        typedef std::shared_ptr<class Texture>		TextureRef;
+        //typedef Texture2dRef							TextureRef;
+    }
 }
 
+// NOTE(hunter): These only seem to work in the imconfig.h file
 // Custom implicit cast operators
 // #ifndef CINDER_IMGUI_NO_IMPLICIT_CASTS
 //     #define IM_VEC2_CLASS_EXTRA                                             \
@@ -69,124 +70,122 @@ namespace ui = ImGui;
 //! cinder imgui namespace
 namespace ImGui {
 
-  struct Options {
-    //! defaults to using the current window, the basic ImGui font and the dark theme
-    Options();
+    struct Options {
+        //! defaults to using the current window, the basic ImGui font and the dark theme
+        Options();
 
-    //! sets the window that will be used to connect the signals and render ImGui
-    Options& window( const ci::app::WindowRef &window );
+        //! sets the window that will be used to connect the signals and render ImGui
+        Options& window( const ci::app::WindowRef &window );
 
-    //! sets the font to use in ImGui
-    Options& font( const ci::fs::path &fontPath, float size );
-    //! sets the list of available fonts to use in ImGui
-    Options& fonts( const std::vector<std::pair<ci::fs::path, float>> &fontPaths );
-    //! sets the font to use in ImGui
-    Options& fontGlyphRanges( const std::string &name, const std::vector<ImWchar> &glyphRanges );
+        //! sets the font to use in ImGui
+        Options& font( const ci::fs::path &fontPath, float size );
+        //! sets the list of available fonts to use in ImGui
+        Options& fonts( const std::vector<std::pair<ci::fs::path, float>> &fontPaths );
+        //! sets the font to use in ImGui
+        Options& fontGlyphRanges( const std::string &name, const std::vector<ImWchar> &glyphRanges );
 
-    //! Global alpha applies to everything in ImGui
-    Options& alpha( float a );
-    //! Padding within a window
-    Options& windowPadding( const ci::Vec2f &padding );
-    //! Minimum window size
-    Options& windowMinSize( const ci::Vec2f &minSize );
-    //! Radius of window corners rounding. Set to 0.0f to have rectangular windows
-    Options& windowRounding( float rounding );
-    //! Padding within a framed rectangle (used by most widgets)
-    Options& framePadding( const ci::Vec2f &padding );
-    //! Radius of frame corners rounding. Set to 0.0f to have rectangular frame (used by most widgets).
-    Options& frameRounding( float rounding );
-    //! Horizontal and vertical spacing between widgets/lines
-    Options& itemSpacing( const ci::Vec2f &spacing );
-    //! Horizontal and vertical spacing between within elements of a composed widget (e.g. a slider and its label)
-    Options& itemInnerSpacing( const ci::Vec2f &spacing );
-    //! Expand bounding box for touch-based system where touch position is not accurate enough (unnecessary for mouse inputs). Unfortunately we don't sort widgets so priority on overlap will always be given to the first widget running. So dont grow this too much!
-    Options& touchExtraPadding( const ci::Vec2f &padding );
-    //! Extra space after auto-fit (double-clicking on resize grip)
-    Options& autoFitPadding( const ci::Vec2f &padding );
-    //! Default alpha of window background, if not specified in ImGui::Begin()
-    Options& windowFillAlphaDefault( float defaultAlpha );
-    //! Horizontal spacing when entering a tree node
-    Options& treeNodeSpacing( float spacing );
-    //! Minimum horizontal spacing between two columns
-    Options& columnsMinSpacing( float minSpacing );
-    //! Width of the vertical scroll bar
-    Options& scrollBarWidth( float width );
+        //! Global alpha applies to everything in ImGui
+        Options& alpha( float a );
+        //! Padding within a window
+        Options& windowPadding( const ci::Vec2f &padding );
+        //! Minimum window size
+        Options& windowMinSize( const ci::Vec2f &minSize );
+        //! Radius of window corners rounding. Set to 0.0f to have rectangular windows
+        Options& windowRounding( float rounding );
+        //! Padding within a framed rectangle (used by most widgets)
+        Options& framePadding( const ci::Vec2f &padding );
+        //! Radius of frame corners rounding. Set to 0.0f to have rectangular frame (used by most widgets).
+        Options& frameRounding( float rounding );
+        //! Horizontal and vertical spacing between widgets/lines
+        Options& itemSpacing( const ci::Vec2f &spacing );
+        //! Horizontal and vertical spacing between within elements of a composed widget (e.g. a slider and its label)
+        Options& itemInnerSpacing( const ci::Vec2f &spacing );
+        //! Expand bounding box for touch-based system where touch position is not accurate enough (unnecessary for mouse inputs). Unfortunately we don't sort widgets so priority on overlap will always be given to the first widget running. So dont grow this too much!
+        Options& touchExtraPadding( const ci::Vec2f &padding );
+        //! Extra space after auto-fit (double-clicking on resize grip)
+        Options& autoFitPadding( const ci::Vec2f &padding );
+        //! Default alpha of window background, if not specified in ImGui::Begin()
+        Options& windowFillAlphaDefault( float defaultAlpha );
+        //! Horizontal spacing when entering a tree node
+        Options& treeNodeSpacing( float spacing );
+        //! Minimum horizontal spacing between two columns
+        Options& columnsMinSpacing( float minSpacing );
+        //! Width of the vertical scroll bar
+        Options& scrollBarWidth( float width );
 
-    //! sets light theme style
-    Options& light();
-    //! sets dark theme style
-    Options& dark();
-    //! sets theme colors
-    Options& themeColors( ImVec4 color0, ImVec4 color1, ImVec4 color2, ImVec4 color3, ImVec4 color4 );
-    
-    //! sets a theme color
-    Options& setThemeColor( ImGuiCol_ option, ImVec4 v ) { mStyle.Colors[option] = v; return *this; }
-    Options& setThemeColor( ImGuiCol_ option, ci::ColorA v ) { mStyle.Colors[option] = v; return *this; }
-    Options& setThemeColor( ImGuiCol_ option, ci::Color v ) { mStyle.Colors[option] = v; return *this; }
+        //! sets light theme style
+        Options& light();
+        //! sets dark theme style
+        Options& dark();
 
-    //! returns the window that will be use to connect the signals and render ImGui
-    ci::app::WindowRef                                  getWindow() const { return mWindow; }
-    //! returns the list of available fonts to use in ImGui
-    const std::vector<std::pair<ci::fs::path, float>>&   getFonts() const { return mFonts; }
-    //! returns the glyph ranges if available for this font
-    const ImWchar*                                      getFontGlyphRanges( const std::string &name ) const;
-    //! returns the window that will be use to connect the signals and render ImGui
-    const ImGuiStyle&                                   getStyle() const { return mStyle; }
+        //! sets a theme color
+        Options& setThemeColor( ImGuiCol_ option, ImVec4 v ) { mStyle.Colors[option] = v; return *this; }
+        Options& setThemeColor( ImGuiCol_ option, ci::ColorA v ) { mStyle.Colors[option] = v; return *this; }
+        Options& setThemeColor( ImGuiCol_ option, ci::Color v ) { mStyle.Colors[option] = v; return *this; }
 
-  protected:
-    ImGuiStyle                                  mStyle;
-    std::vector<std::pair<ci::fs::path, float>>  mFonts;
-    std::map<std::string, std::vector<ImWchar>>  mFontsGlyphRanges;
-    ci::app::WindowRef                          mWindow;
-  };
+        //! returns the window that will be use to connect the signals and render ImGui
+        ci::app::WindowRef                                  getWindow() const { return mWindow; }
+        //! returns the list of available fonts to use in ImGui
+        const std::vector<std::pair<ci::fs::path, float>>&   getFonts() const { return mFonts; }
+        //! returns the glyph ranges if available for this font
+        const ImWchar*                                      getFontGlyphRanges( const std::string &name ) const;
+        //! returns the window that will be use to connect the signals and render ImGui
+        const ImGuiStyle&                                   getStyle() const { return mStyle; }
 
-  //! initializes ImGui and the Renderer
-  void    initialize( const Options &options = Options() );
-  //! connects window signals to imgui events
-  void    connectWindow( ci::app::WindowRef window );
-  //! disconnects window signals from imgui
-  void    disconnectWindow( ci::app::WindowRef window );
+    protected:
+        ImGuiStyle                                  mStyle;
+        std::vector<std::pair<ci::fs::path, float>>  mFonts;
+        std::map<std::string, std::vector<ImWchar>>  mFontsGlyphRanges;
+        ci::app::WindowRef                          mWindow;
+    };
 
-  // Cinder Helpers
-  void Image( const ci::gl::TextureRef &texture, const ImVec2& size, const ImVec2& uv0 = ImVec2( 0, 0 ), const ImVec2& uv1 = ImVec2( 1, 1 ), const ImVec4& tint_col = ImVec4( 1, 1, 1, 1 ), const ImVec4& border_col = ImVec4( 0, 0, 0, 0 ) );
-  bool ImageButton( const ci::gl::TextureRef &texture, const ImVec2& size, const ImVec2& uv0 = ImVec2( 0, 0 ), const ImVec2& uv1 = ImVec2( 1, 1 ), int frame_padding = -1, const ImVec4& bg_col = ImVec4( 0, 0, 0, 1 ), const ImVec4& tint_col = ImVec4( 1, 1, 1, 1 ) );
-  void PushFont( const std::string& name = "" );
+    //! initializes ImGui and the Renderer
+    void    initialize( const Options &options = Options() );
+    //! connects window signals to imgui events
+    void    connectWindow( ci::app::WindowRef window );
+    //! disconnects window signals from imgui
+    void    disconnectWindow( ci::app::WindowRef window );
 
-  // Scoped objects goodness (push the state when created and pop it when destroyed)
-  struct ScopedWindow : public boost::noncopyable {
-    ScopedWindow( const std::string &name = "Debug", bool* opened = NULL, ci::Vec2f size = ci::Vec2f( 0 ), float fillAlpha = -1.0f, ImGuiWindowFlags flags = 0 );
-    ~ScopedWindow();
-  };
-  struct ScopedChild : public boost::noncopyable {
-    ScopedChild( const std::string &name, ci::Vec2f size = ci::Vec2f( 0 ), bool border = false, ImGuiWindowFlags extraFlags = 0 );
-    ~ScopedChild();
-  };
-  struct ScopedFont : public boost::noncopyable {
-    ScopedFont( const std::string &name );
-    ~ScopedFont();
-  };
-  struct ScopedStyleColor : public boost::noncopyable {
-    ScopedStyleColor( ImGuiCol idx, const ImVec4& col );
-    ~ScopedStyleColor();
-  };
-  struct ScopedStyleVar : public boost::noncopyable {
-    ScopedStyleVar( ImGuiStyleVar idx, float val );
-    ScopedStyleVar( ImGuiStyleVar idx, const ImVec2 &val );
-    ~ScopedStyleVar();
-  };
-  struct ScopedItemWidth : public boost::noncopyable {
-    ScopedItemWidth( float itemWidth );
-    ~ScopedItemWidth();
-  };
-  struct ScopedTextWrapPos : public boost::noncopyable {
-    ScopedTextWrapPos( float wrapPosX = 0.0f );
-    ~ScopedTextWrapPos();
-  };
-  struct ScopedId : public boost::noncopyable {
-    ScopedId( const std::string &name );
-    ScopedId( const void *ptrId );
-    ScopedId( const int intId );
-    ~ScopedId();
-  };
+    // Cinder Helpers
+    void Image( const ci::gl::TextureRef &texture, const ImVec2& size, const ImVec2& uv0 = ImVec2( 0, 0 ), const ImVec2& uv1 = ImVec2( 1, 1 ), const ImVec4& tint_col = ImVec4( 1, 1, 1, 1 ), const ImVec4& border_col = ImVec4( 0, 0, 0, 0 ) );
+    bool ImageButton( const ci::gl::TextureRef &texture, const ImVec2& size, const ImVec2& uv0 = ImVec2( 0, 0 ), const ImVec2& uv1 = ImVec2( 1, 1 ), int frame_padding = -1, const ImVec4& bg_col = ImVec4( 0, 0, 0, 1 ), const ImVec4& tint_col = ImVec4( 1, 1, 1, 1 ) );
+    void PushFont( const std::string& name = "" );
+
+    // Scoped objects goodness (push the state when created and pop it when destroyed)
+    struct ScopedWindow : public boost::noncopyable {
+        ScopedWindow( const std::string &name = "Debug", bool* opened = NULL, ci::Vec2f size = ci::Vec2f( 0 ), float fillAlpha = -1.0f, ImGuiWindowFlags flags = 0 );
+        ~ScopedWindow();
+    };
+    struct ScopedChild : public boost::noncopyable {
+        ScopedChild( const std::string &name, ci::Vec2f size = ci::Vec2f( 0 ), bool border = false, ImGuiWindowFlags extraFlags = 0 );
+        ~ScopedChild();
+    };
+    struct ScopedFont : public boost::noncopyable {
+        ScopedFont( const std::string &name );
+        ~ScopedFont();
+    };
+    struct ScopedStyleColor : public boost::noncopyable {
+        ScopedStyleColor( ImGuiCol idx, const ImVec4& col );
+        ~ScopedStyleColor();
+    };
+    struct ScopedStyleVar : public boost::noncopyable {
+        ScopedStyleVar( ImGuiStyleVar idx, float val );
+        ScopedStyleVar( ImGuiStyleVar idx, const ImVec2 &val );
+        ~ScopedStyleVar();
+    };
+    struct ScopedItemWidth : public boost::noncopyable {
+        ScopedItemWidth( float itemWidth );
+        ~ScopedItemWidth();
+    };
+    struct ScopedTextWrapPos : public boost::noncopyable {
+        ScopedTextWrapPos( float wrapPosX = 0.0f );
+        ~ScopedTextWrapPos();
+    };
+    struct ScopedId : public boost::noncopyable {
+        ScopedId( const std::string &name );
+        ScopedId( const void *ptrId );
+        ScopedId( const int intId );
+        ~ScopedId();
+    };
 }
 
