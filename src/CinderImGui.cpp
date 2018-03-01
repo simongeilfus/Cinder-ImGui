51cfdb3253073b554cc9a39c6120b5fec3b11111
Marcus Pingel <marcus@electronicbohemia.com>
2018-02-28 22:30:39 -0500

    Differentiated char type used in keyDown() for Linux. Allows for text input.

--- src/CinderImGui.cpp
+++ src/CinderImGui.cpp
@@ -878,9 +878,13 @@ namespace {
 	void keyDown( ci::app::KeyEvent& event )
 	{
 		ImGuiIO& io = ImGui::GetIO();
-		
-		uint32_t character = event.getCharUtf32();
-		
+
+		#if defined CINDER_LINUX
+			auto character = event.getChar();
+		#else
+			uint32_t character = event.getCharUtf32();
+		#endif
+
 		io.KeysDown[event.getCode()] = true;
 		
 		if ( !event.isAccelDown() && character > 0 && character <= 255 ) {
